6556c57d9c5e3adbf4605defabb33b03f3f69ada
Subham Yadav <subhamy142@gmail.com>
2021-01-08 20:43:58 +0530

    Added cart and its route and contoller

--- app/router.js
+++ app/router.js
@@ -6,4 +6,8 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('not-found',{path:'/*path'});
+  this.route('item',{path:'/item/:item_id'});
+  this.route('cart',{path:'shopping-cart'});
+});
